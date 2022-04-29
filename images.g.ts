c1b0058a8a97c8b1912266c13792fb82bde67c9f
commonboy12 <80496477+commonboy12@users.noreply.github.com>
2022-04-28 21:10:14 -0400

    Update pxt.json, main.blocks, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "electricbogalo":return img`
+. . . 6 . . . . 7 . . 8 7 8 . . 
+. . 7 7 7 . . 7 7 . 8 7 7 . . . 
+. . . 8 7 7 6 7 . 7 7 7 7 . . . 
+. . . 7 . 7 . . . 7 7 7 7 7 7 . 
+7 7 . 7 . 7 7 . . . . . . . . . 
+. 7 . 7 8 . . 7 . 7 7 7 7 7 . . 
+. 7 . 7 . 7 6 7 7 7 6 8 c 7 7 . 
+. 7 . 7 . c c 8 8 8 7 8 c c 7 . 
+. 7 7 7 8 7 c c 7 c 8 7 8 c 7 7 
+. 7 7 7 8 7 7 c 7 7 8 7 c c . 7 
+7 . 7 7 8 8 6 c 6 7 7 7 8 8 7 . 
+7 7 . c c 8 8 c 7 8 6 7 7 8 7 . 
+. 7 8 8 8 7 c c 7 6 7 7 7 8 . . 
+. 7 7 7 7 7 7 6 7 7 7 7 7 7 7 . 
+. 7 7 7 7 8 7 7 7 7 7 7 7 . . . 
+. . 7 . 8 . . . . . 8 7 7 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
